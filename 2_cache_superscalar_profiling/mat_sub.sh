43162a468846afa3da7195b69035ef24bcd87712
suhipek <dm98@sohu.com>
2022-03-07 23:39:06 +0800

    bug fixes

--- 2_cache_superscalar_profiling/mat_sub.sh
+++ 2_cache_superscalar_profiling/mat_sub.sh
@@ -1,7 +1,6 @@
 # mat_sub.sh
 # !/bin/sh
 pssh -h $PBS_NODEFILE mkdir -p /home/s2010056/2_cache_superscalar_profiling l>&2
-pscp -h $PBS_NODEFILE /home/s2010056/NKU_parallel_programming/2_cache_superscalar_profiling/mar_vec_gen /home/s2010056/2_cache_superscalar_profiling l>&2
 pscp -h $PBS_NODEFILE /home/s2010056/NKU_parallel_programming/2_cache_superscalar_profiling/matrix_product /home/s2010056/2_cache_superscalar_profiling l>&2
-/s2010056/2_cache_superscalar_profiling/mar_vec_gen
+pscp -h $PBS_NODEFILE /home/s2010056/NKU_parallel_programming/2_cache_superscalar_profiling/mar_vec.dat /home/s2010056/2_cache_superscalar_profiling l>&2
 /s2010056/2_cache_superscalar_profiling/matrix_product
