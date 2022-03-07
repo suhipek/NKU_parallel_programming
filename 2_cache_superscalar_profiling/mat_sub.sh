8cdb1067139aad4f1364edd26af6c4ad52898058
suhipek <dm98@sohu.com>
2022-03-08 00:20:54 +0800

    笑死,忘记编译了还以为是脚本bug

--- 2_cache_superscalar_profiling/mat_sub.sh
+++ 2_cache_superscalar_profiling/mat_sub.sh
@@ -1,6 +1,6 @@
 # mat_sub.sh
 # !/bin/sh
-pssh -h $PBS_NODEFILE mkdir -p /home/s2010056/NKU_parallel_programming/2_cache_superscalar_profiling l>&2
-pscp -h $PBS_NODEFILE /home/s2010056/NKU_parallel_programming/2_cache_superscalar_profiling/matrix_product /home/s2010056/NKU_parallel_programming/2_cache_superscalar_profiling l>&2
-pscp -h $PBS_NODEFILE /home/s2010056/NKU_parallel_programming/2_cache_superscalar_profiling/mar_vec.dat /home/s2010056/NKU_parallel_programming/2_cache_superscalar_profiling l>&2
-/home/s2010056/NKU_parallel_programming/2_cache_superscalar_profiling/matrix_product
+pssh -h $PBS_NODEFILE mkdir -p /home/s2010056/2_cache_superscalar_profiling 1>&2
+pscp -h $PBS_NODEFILE /home/s2010056/NKU_parallel_programming/2_cache_superscalar_profiling/matrix_product /home/s2010056/2_cache_superscalar_profiling 1>&2
+pscp -h $PBS_NODEFILE /home/s2010056/NKU_parallel_programming/2_cache_superscalar_profiling/mar_vec.dat /home/s2010056/2_cache_superscalar_profiling 1>&2
+/home/s2010056/2_cache_superscalar_profiling/matrix_product
