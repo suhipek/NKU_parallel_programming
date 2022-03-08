f842d5acf13258c9e9c2c1ca54accbfe6a206a77
suhipek <dm98@sohu.com>
2022-03-08 21:07:59 +0800

    脚本生成代码实现无依赖展开循环

--- 2_cache_superscalar_profiling/cumulative.cpp
+++ 2_cache_superscalar_profiling/cumulative.cpp
@@ -3,73 +3,33 @@
 #include <iomanip>
 #include <time.h>
 #include <sys/time.h>
+#include "unroll_sum.h"
 
 #define USE_FIXED_N
 #define N 1048576
-#define REPT 500
+// N MUST BE A POWER OF 2
+#define REPT 1000
 
 using namespace std;
 
-int common_algo(int *arr, int len)
+void test(int (*func)(int*, int), char* msg, int* arr, int len)
 {
-    int total = 0;
-    for (int i = 0; i < len; i++)
-        total += arr[i];
-    return total;
+    timespec start, end;
+    double time_used = 0;
+    clock_gettime(CLOCK_REALTIME, &start);
+    for (int i = 0; i < REPT; i++)
+        func(arr, len);
+    clock_gettime(CLOCK_REALTIME, &end);
+    time_used += end.tv_sec - start.tv_sec;
+    time_used += double(end.tv_nsec - start.tv_nsec) / 1000000000;
+    cout << msg << ": " << time_used << endl;
 }
 
-int dual_algo(int *arr, int len)
+void test(int (*func)(int*, int), const char* msg, int* arr, int len)
 {
-    int total1 = 0, total2 = 0;
-    for (int i = 0; i < len; i += 2)
-    {
-        total1 += arr[i];
-        total2 += arr[i + 1];
-    }
-    return total1 + total2;
+    return test(func, (char*)msg, arr, len);
 }
 
-int quad_algo(int *arr, int len)
-{
-    int total1 = 0, total2 = 0, total3 = 0, total4 = 0;
-    for (int i = 0; i < len; i += 4)
-    {
-        total1 += arr[i];
-        total2 += arr[i + 1];
-        total3 += arr[i + 2];
-        total4 += arr[i + 3];
-    }
-    return total1 + total2 + total3 + total4;
-}
-
-#define SUM             \
-    total[m] += arr[j]; \
-    j++;
-#define SUM2 SUM SUM
-#define SUM4 SUM2 SUM2
-#define SUM8 SUM4 SUM4
-#define SUM16 SUM8 SUM8
-#define SUM32 SUM16 SUM16
-#define SUM64 SUM32 SUM32
-#define SUM128 SUM64 SUM64
-#define SUM256 SUM128 SUM128
-#define SUM512 SUM256 SUM256
-#define SUM1024 SUM512 SUM512
-#define SUM2048 SUM1024 SUM1024
-#define SUM4096 SUM2048 SUM2048
-#define SUM8192 SUM4096 SUM4096
-
-int unroll_algo(int *arr, int len)
-{
-    int j, m, total[len / 8192] = {0};
-    for (int i = 0; i < len - 8192; i += 8192)
-    {
-        j = i;
-        m = i / 8192;
-        SUM8192
-    }
-    return common_algo(total, len / 8192);
-}
 
 int main()
 {
@@ -78,47 +38,20 @@ int main()
     data.read((char *)arr, N * sizeof(int));
     data.close();
 
-    timespec start, end;
-    double time_used = 0;
-    clock_gettime(CLOCK_REALTIME, &start);
-    for (int i = 0; i < REPT; i++)
-        common_algo(arr, N);
-    clock_gettime(CLOCK_REALTIME, &end);
-    time_used += end.tv_sec - start.tv_sec;
-    time_used += double(end.tv_nsec - start.tv_nsec) / 1000000000;
-    cout << "common_algo: " << time_used << endl;
-
-    time_used = 0;
-    clock_gettime(CLOCK_REALTIME, &start);
-    for (int i = 0; i < REPT; i++)
-        dual_algo(arr, N);
-    clock_gettime(CLOCK_REALTIME, &end);
-    time_used += end.tv_sec - start.tv_sec;
-    time_used += double(end.tv_nsec - start.tv_nsec) / 1000000000;
-    cout << "dual_algo: " << time_used << endl;
-
-    time_used = 0;
-    clock_gettime(CLOCK_REALTIME, &start);
-    for (int i = 0; i < REPT; i++)
-        quad_algo(arr, N);
-    clock_gettime(CLOCK_REALTIME, &end);
-    time_used += end.tv_sec - start.tv_sec;
-    time_used += double(end.tv_nsec - start.tv_nsec) / 1000000000;
-    cout << "quad_algo: " << time_used << endl;
-
-    time_used = 0;
-    clock_gettime(CLOCK_REALTIME, &start);
-    for (int i = 0; i < REPT; i++)
-        unroll_algo(arr, N);
-    clock_gettime(CLOCK_REALTIME, &end);
-    time_used += end.tv_sec - start.tv_sec;
-    time_used += double(end.tv_nsec - start.tv_nsec) / 1000000000;
-    cout << "unroll_algo: " << time_used << endl;
-
-    int ret_common = common_algo(arr, N);
-    int ret_dual = dual_algo(arr, N);
-
-    if (ret_dual != ret_common)
-        cout << "results mismatch" << endl;
+    test(common_algo, "common_algo", arr, N);
+    test(unroll_algo_2, "unroll_algo_2", arr, N);
+    test(unroll_algo_4, "unroll_algo_4", arr, N);
+    test(unroll_algo_8, "unroll_algo_8", arr, N);
+    test(unroll_algo_16, "unroll_algo_16", arr, N);
+    test(unroll_algo_32, "unroll_algo_32", arr, N);
+    test(unroll_algo_64, "unroll_algo_64", arr, N);
+    test(unroll_algo_128, "unroll_algo_128", arr, N);
+    test(unroll_algo_256, "unroll_algo_256", arr, N);
+    test(unroll_algo_512, "unroll_algo_512", arr, N);
+    test(unroll_algo_1024, "unroll_algo_1024", arr, N);
+    test(unroll_algo_2048, "unroll_algo_2048", arr, N);
+    test(unroll_algo_4096, "unroll_algo_4096", arr, N);
+    test(unroll_algo_8192, "unroll_algo_8192", arr, N);
+    
     return 0;
 }
---
+++ 2_cache_superscalar_profiling/unroll_sum.h
@@ -0,0 +1,16 @@
+
+
+int common_algo(int *arr, int len);
+int unroll_algo_2(int *arr, int len);
+int unroll_algo_4(int *arr, int len);
+int unroll_algo_8(int *arr, int len);
+int unroll_algo_16(int *arr, int len);
+int unroll_algo_32(int *arr, int len);
+int unroll_algo_64(int *arr, int len);
+int unroll_algo_128(int *arr, int len);
+int unroll_algo_256(int *arr, int len);
+int unroll_algo_512(int *arr, int len);
+int unroll_algo_1024(int *arr, int len);
+int unroll_algo_2048(int *arr, int len);
+int unroll_algo_4096(int *arr, int len);
+int unroll_algo_8192(int *arr, int len);
