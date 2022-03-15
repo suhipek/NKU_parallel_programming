77cdbd247969e550139a94365859db8f1300014a
suhipek <dm98@sohu.com>
2022-03-15 13:00:44 +0800

    对不同数据大小多次测试

--- 2_cache_superscalar_profiling/mat_timing.sh
+++ 2_cache_superscalar_profiling/mat_timing.sh
@@ -2,7 +2,11 @@
 # !/bin/sh
 timestr=$(date +%m_%d_%H_%M)
 pssh -h $PBS_NODEFILE mkdir -p /home/s2010056/2_cache_superscalar_profiling 1>&2
-pscp -h $PBS_NODEFILE /home/s2010056/NKU_parallel_programming/2_cache_superscalar_profiling/matrix_product /home/s2010056/2_cache_superscalar_profiling 1>&2
+pscp -h $PBS_NODEFILE /home/s2010056/NKU_parallel_programming/2_cache_superscalar_profiling/matrix_product.cpp /home/s2010056/2_cache_superscalar_profiling 1>&2
 pscp -h $PBS_NODEFILE /home/s2010056/NKU_parallel_programming/2_cache_superscalar_profiling/mar_vec.dat /home/s2010056/2_cache_superscalar_profiling 1>&2
-perf record -e L1-dcache-load-misses,L1-dcache-loads,L1-dcache-store-misses,L1-dcache-stores -g -o /home/s2010056/2_cache_superscalar_profiling/perf_$timestr.data /home/s2010056/2_cache_superscalar_profiling/matrix_product
-pscp -h $PBS_NODEFILE /home/s2010056/2_cache_superscalar_profiling/perf_$timestr.data /home/s2010056/NKU_parallel_programming/2_cache_superscalar_profiling/ 1>&2
+
+for i in {1..150}
+do 
+    g++ -DN=$[10*i] /home/s2010056/2_cache_superscalar_profiling/matrix_product.cpp -o /home/s2010056/2_cache_superscalar_profiling/matrix_product
+    /home/s2010056/2_cache_superscalar_profiling/matrix_product
+done
