edce36f6e79f1cec320a541c50b8a82bbb253526
suhipek <dm98@sohu.com>
2022-04-10 21:07:40 +0800

    fix

--- 3_simd/gauss_sub.sh
+++ 3_simd/gauss_sub.sh
@@ -2,6 +2,6 @@
 # !/bin/sh
 timestr=$(date +%m_%d_%H_%M)
 pssh -h $PBS_NODEFILE mkdir -p /home/s2010056/3_simd 1>&2
-pscp -h $PBS_NODEFILE /home/s2010056/NKU_parallel_programming/3_simd/gauss.cpp /home/s2010056/3_simd 1>&2
+pscp -h $PBS_NODEFILE /home/s2010056/NKU_parallel_programming/3_simd/gauss /home/s2010056/3_simd 1>&2
 pscp -h $PBS_NODEFILE /home/s2010056/NKU_parallel_programming/3_simd/gauss.dat /home/s2010056/3_simd 1>&2
 /home/s2010056/3_simd/gauss
