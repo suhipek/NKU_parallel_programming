4524e6c3cda62aff5fcf4ed6d51aa8c04005d1cb
suhipek <dm98@sohu.com>
2022-04-12 20:51:49 +0800

    不同规模

--- 3_simd/gauss_sub.sh
+++ 3_simd/gauss_sub.sh
@@ -2,6 +2,11 @@
 # !/bin/sh
 timestr=$(date +%m_%d_%H_%M)
 pssh -h $PBS_NODEFILE mkdir -p /home/s2010056/3_simd 1>&2
-pscp -h $PBS_NODEFILE /home/s2010056/NKU_parallel_programming/3_simd/gauss /home/s2010056/3_simd 1>&2
-pscp -h $PBS_NODEFILE /home/s2010056/NKU_parallel_programming/3_simd/gauss.dat /home/s2010056/3_simd 1>&2
-/home/s2010056/3_simd/gauss
+pscp -h $PBS_NODEFILE /home/s2010056/NKU_parallel_programming/3_simd/gauss.cpp /home/s2010056/simd 1>&2
+pscp -h $PBS_NODEFILE /home/s2010056/NKU_parallel_programming/3_simd/gauss.dat /home/s2010056/simd 1>&2
+cd /home/s2010056/simd
+for i in {1..32}
+do 
+    g++ -march=native -DN=$[64*i] ./gauss.cpp -o ./gauss_test
+    ./gauss_test >> ./gauss_timing_$timestr.csv
+done
