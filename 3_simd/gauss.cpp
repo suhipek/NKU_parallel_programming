e91677ca4984c7118163462bcbb848e47245af0b
suhipek <dm98@sohu.com>
2022-04-12 17:01:48 +0800

    avx和对齐

--- 3_simd/gauss.cpp
+++ 3_simd/gauss.cpp
@@ -5,18 +5,25 @@
 #include <sys/time.h>
 #include <cmath>
 
-//#include <arm_neon.h>
-#include "NEON_2_SSE.h"
+#ifdef __arm__
+#include <arm_neon.h>
+#endif
 
-#define REPT 100
-#define N 256
+#ifdef __amd64__
+#include <immintrin.h>
+#include "NEON_2_SSE.h"
+#define USE_SSE4
+#endif
+
+#define REPT 1
+#define N 1024
 #define ele_t float
 //#define DEBUG
 
 using namespace std;
 
+ele_t new_mat[N][N] __attribute__((aligned(32)));
 ele_t mat[N][N];
-ele_t new_mat[N][N];
 
 void test(void (*func)(ele_t[N][N], int), const char *msg, ele_t mat[N][N], int len)
 {
@@ -35,7 +42,7 @@ void test(void (*func)(ele_t[N][N], int), const char *msg, ele_t mat[N][N], int 
 
 void LU(ele_t mat[N][N], int n)
 {
-    //ele_t new_mat[N][N];
+    // ele_t new_mat[N][N];
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
             new_mat[i][j] = mat[i][j];
@@ -58,12 +65,13 @@ void LU(ele_t mat[N][N], int n)
             cout << new_mat[i][j] << ' ';
         cout << endl;
     }
+    cout << endl;
 #endif
 }
 
 void LU_simd(ele_t mat[N][N], int n)
 {
-    //ele_t new_mat[N][N];
+    // ele_t new_mat[N][N];
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
             new_mat[i][j] = mat[i][j];
@@ -74,13 +82,16 @@ void LU_simd(ele_t mat[N][N], int n)
             if (new_mat[i][i] == 0)
                 continue;
             ele_t div = new_mat[j][i] / new_mat[i][i];
-            float32x4_t div4 = vmovq_n_f32(-1 * div);
+            float32x4_t div4 = vmovq_n_f32(div);
+            float32x4_t mat_j;
+            float32x4_t mat_i;
+            float32x4_t res;
             // cout << new_mat[j][i] << '/' << new_mat[i][i] << '=' << div << endl;
             for (int k = i; k < n; k += 4)
             {
-                float32x4_t mat_j = vld1q_f32(new_mat[j] + k);
-                float32x4_t mat_i = vld1q_f32(new_mat[i] + k);
-                float32x4_t res = vmlaq_f32(mat_j, div4, mat_i);
+                mat_j = vld1q_f32(new_mat[j] + k);
+                mat_i = vld1q_f32(new_mat[i] + k);
+                res = vmlsq_f32(mat_j, div4, mat_i);
                 vst1q_f32(new_mat[j] + k, res);
             }
         }
@@ -92,6 +103,83 @@ void LU_simd(ele_t mat[N][N], int n)
             cout << new_mat[i][j] << ' ';
         cout << endl;
     }
+    cout << endl;
+#endif
+}
+
+void LU_avx_aligned(ele_t mat[N][N], int n)
+{
+    // ele_t new_mat[N][N];
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            new_mat[i][j] = mat[i][j];
+
+    for (int i = 0; i < n; i++)
+        for (int j = i + 1; j < n; j++)
+        {
+            if (new_mat[i][i] == 0)
+                continue;
+            ele_t div = new_mat[j][i] / new_mat[i][i];
+            __m256 div8 = _mm256_set1_ps(div);
+            __m256 mat_j;
+            __m256 mat_i;
+            __m256 res;
+            // cout << new_mat[j][i] << '/' << new_mat[i][i] << '=' << div << endl;
+            for (int k = i / 8 * 8; k < n; k += 8)
+            {
+                mat_j = _mm256_load_ps(new_mat[j] + k);
+                mat_i = _mm256_load_ps(new_mat[i] + k);
+                res = _mm256_fnmadd_ps(mat_i, div8, mat_j);
+                _mm256_store_ps(new_mat[j] + k, res);
+            }
+        }
+
+#ifdef DEBUG
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+            cout << new_mat[i][j] << ' ';
+        cout << endl;
+    }
+    cout << endl;
+#endif
+}
+
+void LU_avx(ele_t mat[N][N], int n)
+{
+    // ele_t new_mat[N][N];
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            new_mat[i][j] = mat[i][j];
+
+    for (int i = 0; i < n; i++)
+        for (int j = i + 1; j < n; j++)
+        {
+            if (new_mat[i][i] == 0)
+                continue;
+            ele_t div = new_mat[j][i] / new_mat[i][i];
+            __m256 div8 = _mm256_set1_ps(div);
+            __m256 mat_j;
+            __m256 mat_i;
+            __m256 res;
+            // cout << new_mat[j][i] << '/' << new_mat[i][i] << '=' << div << endl;
+            for (int k = i; k < n; k += 8)
+            {
+                mat_j = _mm256_loadu_ps(new_mat[j] + k);
+                mat_i = _mm256_loadu_ps(new_mat[i] + k);
+                res = _mm256_fnmadd_ps(mat_i, div8, mat_j);
+                _mm256_storeu_ps(new_mat[j] + k, res);
+            }
+        }
+
+#ifdef DEBUG
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+            cout << new_mat[i][j] << ' ';
+        cout << endl;
+    }
+    cout << endl;
 #endif
 }
 
@@ -102,12 +190,27 @@ int main()
     data.read((char *)mat, N * N * sizeof(ele_t));
     data.close();
 
+#ifdef NO_ALIGN_INFO
+    cout << "new_mat addr:" << &new_mat << "    ";
+    cout << "% 256 == " << (long long)new_mat % 256 << endl;
+    cout << "alignof(new_mat): " << alignof(new_mat) << endl;
+#endif
+
 #ifndef DEBUG
     test(LU, "commone algo:", mat, N);
-    test(LU_simd, "optimized algo:", mat, N);
+    test(LU_simd, "SSE:", mat, N);
+#ifdef __amd64__
+    test(LU_avx, "AVX:", mat, N);
+    test(LU_avx_aligned, "AVX Aligned:", mat, N);
+#endif
+    cout << endl;
 #else
     LU(mat, N);
     LU_simd(mat, N);
+#ifdef __amd64__
+    LU_avx(mat, N);
+    LU_avx_aligned(mat, N);
+#endif
 #endif
 
     return 0;
