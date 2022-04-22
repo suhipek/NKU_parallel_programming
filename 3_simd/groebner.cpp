f8b96a251ce93073cb292c2368c6ea76de109236
suhipek <dm98@sohu.com>
2022-04-22 10:42:36 +0800

    完成AVX不对齐加速

--- 3_simd/groebner.cpp
+++ 3_simd/groebner.cpp
@@ -17,25 +17,52 @@
 #define USE_SSE4
 #endif
 
-#ifndef DATA
-#define DATA "./Groebner/1_130_22_8/"
-#define COL 130
-#define ELE 22
-#define ROW 8
-#endif
+// #ifndef DATA
+// #define DATA "./Groebner/1_130_22_8/"
+// #define COL 130
+// #define ELE 22
+// #define ROW 8
+// #endif
 
+// #ifndef DATA
+// #define DATA "./Groebner/2_254_106_53/"
+// #define COL 254
+// #define ELE 106
+// #define ROW 53
+// #endif
+
+// #ifndef DATA
+// #define DATA "./Groebner/3_562_170_53/"
+// #define COL 562
+// #define ELE 170
+// #define ROW 53
+// #endif
+
+// #ifndef DATA
+// #define DATA "./Groebner/4_1011_539_263/"
+// #define COL 1011
+// #define ELE 539
+// #define ROW 263
+// #endif
+
+#ifndef DATA
+#define DATA "./Groebner/6_3799_2759_1953/"
+#define COL 3799
+#define ELE 2759
+#define ROW 1953
+#endif
 
 #define mat_t unsigned int
 #define mat_L 32
-#define REPT 10000
+#define REPT 4
 
 using namespace std;
 
 mat_t ele[COL][COL / mat_L + 1] = {0};
 mat_t row[ROW][COL / mat_L + 1] = {0};
 
-mat_t ele_tmp[COL][COL / mat_L + 1] = {0};
-mat_t row_tmp[ROW][COL / mat_L + 1] = {0};
+mat_t ele_tmp[COL][COL / mat_L + 1] __attribute__((aligned(64))) = {0};
+mat_t row_tmp[ROW][COL / mat_L + 1] __attribute__((aligned(64))) = {0};
 
 void test(void (*func)(mat_t[COL][COL / mat_L + 1], mat_t[ROW][COL / mat_L + 1]), const char *msg)
 {
@@ -80,6 +107,7 @@ void groebner(mat_t ele[COL][COL / mat_L + 1], mat_t row[ROW][COL / mat_L + 1])
 #ifdef DEBUG
     for (int i = 0; i < ROW; i++)
     {
+        cout << i << ": ";
         for (int j = COL; j >= 0; j--)
             if (row_tmp[i][j / mat_L] & ((mat_t)1 << (j % mat_L)))
                 cout << j << ' ';
@@ -106,9 +134,9 @@ void groebner_simd(mat_t ele[COL][COL / mat_L + 1], mat_t row[ROW][COL / mat_L +
                     {
                         row_i = vld1q_u32(row_tmp[i] + p * 4);
                         ele_j = vld1q_u32(ele_tmp[j] + p * 4);
-                        vst1q_u32(row_tmp[i] + p, veorq_u32(row_i, ele_j))
+                        vst1q_u32(row_tmp[i] + p * 4, veorq_u32(row_i, ele_j))
                     }
-                    for (int k = COL / 128 * 4; k < COL / mat_L + 1; k++)
+                    for (int k = COL / 128 * 4; k <= COL / mat_L; k++)
                         row_tmp[i][k] ^= ele_tmp[j][k];
                 }
                 else
@@ -123,6 +151,51 @@ void groebner_simd(mat_t ele[COL][COL / mat_L + 1], mat_t row[ROW][COL / mat_L +
 #ifdef DEBUG
     for (int i = 0; i < ROW; i++)
     {
+        cout << i << ": ";
+        for (int j = COL; j >= 0; j--)
+            if (row_tmp[i][j / mat_L] & ((mat_t)1 << (j % mat_L)))
+                cout << j << ' ';
+        cout << endl;
+    }
+#endif
+}
+
+void groebner_avx(mat_t ele[COL][COL / mat_L + 1], mat_t row[ROW][COL / mat_L + 1])
+{
+    // ele=消元子，row=被消元行
+    memcpy(ele_tmp, ele, sizeof(mat_t) * COL * (COL / mat_L + 1));
+    memcpy(row_tmp, row, sizeof(mat_t) * ROW * (COL / mat_L + 1));
+    __m256i row_i, ele_j;
+    for (int i = 0; i < ROW; i++)
+    {
+        for (int j = COL; j >= 0; j--)
+        {
+            if (row_tmp[i][j / mat_L] & ((mat_t)1 << (j % mat_L)))
+            {
+                if (ele_tmp[j][j / mat_L] & ((mat_t)1 << (j % mat_L)))
+                {
+                    for (int p = 0; p < COL / 256; p++)
+                    {
+                        row_i = _mm256_loadu_si256((__m256i *)(row_tmp[i] + p * 8));
+                        ele_j = _mm256_loadu_si256((__m256i *)(ele_tmp[j] + p * 8));
+                        _mm256_storeu_si256((__m256i *)(row_tmp[i] + p * 8), _mm256_xor_si256(row_i, ele_j));
+                    }
+                    for (int k = COL / 256 * 8; k <= COL / mat_L; k++)
+                        row_tmp[i][k] ^= ele_tmp[j][k];
+                }
+                else
+                {
+                    memcpy(ele_tmp[j], row_tmp[i], (COL / mat_L + 1) * sizeof(mat_t));
+                    break;
+                }
+            }
+        }
+    }
+
+#ifdef DEBUG
+    for (int i = 0; i < ROW; i++)
+    {
+        cout << i << ": ";
         for (int j = COL; j >= 0; j--)
             if (row_tmp[i][j / mat_L] & ((mat_t)1 << (j % mat_L)))
                 cout << j << ' ';
@@ -159,11 +232,14 @@ int main()
 
 #ifdef DEBUG
     groebner(ele, row);
-    cout << endl << endl;
+    cout << endl << "end" << endl;
     groebner_simd(ele, row);
+    cout << endl << "end" << endl;
+    groebner_avx(ele, row);
 #else
     test(groebner, "common");
     test(groebner_simd, "simd");
+    test(groebner_avx, "avx");
 #endif
 
     // groebner(ele, row);
