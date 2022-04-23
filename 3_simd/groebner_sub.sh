ecbcf4a1d257d3ce8fdecc6dd3eaa01b76f5a7d8
suhipek <dm98@sohu.com>
2022-04-23 21:59:34 +0800

    bug fix

--- 3_simd/groebner_sub.sh
+++ 3_simd/groebner_sub.sh
@@ -2,7 +2,7 @@
 # !/bin/sh
 timestr=$(date +%m_%d_%H_%M)
 pssh -h $PBS_NODEFILE mkdir -p /home/s2010056/3_simd 1>&2
-pscp -h $PBS_NODEFILE /home/s2010056/NKU_parallel_programming/3_simd/groebner.cpp /home/s2010056/simd 1>&2
+pscp -h $PBS_NODEFILE /home/s2010056/NKU_parallel_programming/3_simd/groebner.cpp /home/s2010056/3_simd 1>&2
 
 data_path="/home/data/Groebner/"
 cd /home/s2010056/3_simd
