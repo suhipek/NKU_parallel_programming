1208d0a62f331527cfe33b4de91d64756a9cf9aa
suhipek <dm98@sohu.com>
2022-05-18 16:54:49 +0800

    pbs脚本

---
+++ 4_pthread/sub_groebner.sh
@@ -0,0 +1,26 @@
+timestr=$(date +%m_%d_%H_%M)
+pssh -h $PBS_NODEFILE mkdir -p /home/s2010056/4_pthread 1>&2
+pscp -h $PBS_NODEFILE /home/s2010056/NKU_parallel_programming/4_pthread/groebner.cpp /home/s2010056/4_pthread 1>&2
+data_path="/home/data/Groebner/"
+num_th=("1" "4" "8" "12" "16" "20")
+
+for file in $(ls ${data_path}); do
+    if [ "$file" == "README.txt" ]; then
+        continue
+    else
+        attr=(${file//_/ })
+        # echo ${arrIN[1]}
+        if [ "${attr[0]}" == "8" ]; then
+            break
+        fi
+        echo "${data_path}${file}/"
+        for j in {0..5}; do
+            echo ${num_th[j]}"threads"
+            g++ -march=native -w -pthread -DDATA=\"${data_path}${file}/\" \
+                -DCOL=${attr[1]} -DELE=${attr[2]} -DROW=${attr[3]} -DNUM_THREADS=$((${num_th[j]})) \
+                /home/s2010056/4_pthread/groebner.cpp -o /home/s2010056/4_pthread/groebner
+            /home/s2010056/4_pthread/groebner >>/home/s2010056/4_pthread/groebner_$timestr.csv
+        done
+        echo '' >>/home/s2010056/4_pthread/groebner_$timestr.csv
+    fi
+done
