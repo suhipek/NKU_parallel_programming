964960dc13084e414e2d55a6e98964da8a14dc12
suhipek <dm98@sohu.com>
2022-05-18 15:15:20 +0800

    计时脚本

--- 4_pthread/groebner_timing.sh
+++ 4_pthread/groebner_timing.sh
@@ -7,16 +7,11 @@ for file in $(ls ${data_path}); do
     else
         attr=(${file//_/ })
         # echo ${arrIN[1]}
-        if [ "${attr[0]}" == "8" ]; then
+        if [ "${attr[0]}" == "9" ]; then
             break
         fi
         echo "${data_path}${file}/"
-        g++ -march=native -w -DDATA=\"${data_path}${file}/\" \
-            -DCOL=${attr[1]} -DELE=${attr[2]} -DROW=${attr[3]} \
-            ./groebner.cpp -o ./groebner
-        ./groebner >>groebner_$timestr.csv
-        echo "unaligned finished"
-        g++ -march=native -w -DALIGN -DDATA=\"${data_path}${file}/\" \
+        g++ -march=native -w -pthread -DDATA=\"${data_path}${file}/\" \
             -DCOL=${attr[1]} -DELE=${attr[2]} -DROW=${attr[3]} \
             ./groebner.cpp -o ./groebner
         ./groebner >>groebner_$timestr.csv
