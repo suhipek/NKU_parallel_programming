fb5f2e3fdde43fbe388a5b82d5d7e168648c4c55
suhipek <dm98@sohu.com>
2022-07-07 20:28:06 +0800

    bug fix

--- 6_mpi/gauss.cpp
+++ 6_mpi/gauss.cpp
@@ -14,14 +14,14 @@ using namespace std;
 #define ZERO (float)1e-5
 
 #ifndef N
-#define N 16
+#define N 2048
 #endif
 
 #ifndef DATA_PATH
 #define DATA_PATH "/home/suhipek/NKU_parallel_programming/gauss.dat"
 #endif
 
-#define DEBUG
+// #define DEBUG
 
 int world_size, world_rank;
 
@@ -29,7 +29,7 @@ void run_master(ele_t *_mat)
 {
     int NUM_THREADS = world_size - 1;
     ele_t(*mat)[N] = (ele_t(*)[N])_mat;
-
+    cout << endl;
     for (int i = 0; i < N; i++)
     {
         int n_lines = 0;
@@ -37,17 +37,17 @@ void run_master(ele_t *_mat)
         {
             n_lines = (N - i - 1) / (world_size - 1);
         }
-        MPI_Bcast(&i, 1, MPI_INT, 0, MPI_COMM_WORLD);
-        MPI_Bcast(&n_lines, 1, MPI_INT, 0, MPI_COMM_WORLD);
+        // MPI_Bcast(&i, 1, MPI_INT, 0, MPI_COMM_WORLD);
         MPI_Bcast(mat[i], sizeof(ele_t) * N, MPI_BYTE, 0, MPI_COMM_WORLD);
         for (int th = 1; th < world_size; th++)
         {
             MPI_Send(mat[i + 1 + (th - 1) * n_lines], sizeof(ele_t) * N * n_lines, MPI_BYTE, th, 0, MPI_COMM_WORLD);
         }
         int offset = NUM_THREADS ? NUM_THREADS * ((N - i - 1) / NUM_THREADS) : 0;
+#pragma omp parallel for
         for (int j = i + 1 + offset; j < N; j++)
         {
-            if (mat[i][i] == 0)
+            if (abs(mat[i][i]) < ZERO)
                 continue;
             ele_t div = mat[j][i] / mat[i][i];
             for (int k = i; k < N; k++)
@@ -55,7 +55,10 @@ void run_master(ele_t *_mat)
         }
         for (int th = 1; th < world_size; th++)
         {
-            MPI_Recv(mat[i + 1 + (th - 1) * n_lines], sizeof(ele_t) * N * n_lines, MPI_BYTE, th, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            if (n_lines)
+                MPI_Recv(mat[i + 1 + (th - 1) * n_lines], sizeof(ele_t) * N * n_lines, MPI_BYTE, th, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            else
+                MPI_Recv(nullptr, sizeof(ele_t) * N * n_lines, MPI_BYTE, th, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         }
     }
     int i = -1;
@@ -73,32 +76,28 @@ void run_master(ele_t *_mat)
 
 void run_slave()
 {
-    int i_pivot;      // 枢轴位置
+    // int i;      // 枢轴位置
     int n_lines;      // 需要消元的行数
     ele_t lines_i[N]; // 当前消元行
 
-    while (true)
+    for (int i = 0; i < N; i++)
     {
-        MPI_Bcast(&i_pivot, 1, MPI_INT, 0, MPI_COMM_WORLD);
-        if (i_pivot == -1)
-            break;
-        MPI_Bcast(&n_lines, 1, MPI_INT, 0, MPI_COMM_WORLD);
+        n_lines = (N - i - 1) / (world_size - 1);
         MPI_Bcast(lines_i, sizeof(ele_t) * N, MPI_BYTE, 0, MPI_COMM_WORLD);
-        // printf("%d: pivot: %d, lines: %d\n", world_rank, i_pivot, n_lines);
-
+        // printf("%d: pivot: %d, lines: %d\n", world_rank, i, n_lines);
         ele_t(*mat)[N] = (ele_t(*)[N])malloc(n_lines * N * sizeof(ele_t));
         MPI_Recv(mat, n_lines * N * sizeof(ele_t), MPI_BYTE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
+#pragma omp parallel for
         for (int j = 0; j < n_lines; j++)
         {
-            if (abs(mat[j][i_pivot]) < ZERO) // 枢轴为0，不需要消元
+            if (abs(lines_i[i]) < ZERO) // 枢轴为0，不需要消元
                 continue;
-            ele_t div = mat[j][i_pivot] / lines_i[i_pivot];
-            for (int k = i_pivot; k < N; k++)
+            ele_t div = mat[j][i] / lines_i[i];
+            for (int k = i; k < N; k++)
                 mat[j][k] -= lines_i[k] * div;
         }
+        MPI_Send(mat, n_lines * N * sizeof(ele_t), MPI_BYTE, 0, 1, MPI_COMM_WORLD);
         free(mat);
-        MPI_Send(mat, n_lines * N * sizeof(ele_t), MPI_BYTE, 0, 0, MPI_COMM_WORLD);
     }
 }
 
